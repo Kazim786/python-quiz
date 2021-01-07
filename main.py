1545c54e487aae5723394cf9d027d5081d3fd4f7
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-06 21:06:29 -0600

    for loop and appending

---
+++ main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+#for loop in python
+# all_fruits = ["apple",
+# "banana", "orange"]
+# for fruit in all_fruits:
+# print(fruit)
+
+#Appending to a list
+# all_fruits = ["apple",
+# "banana", "orange"]
+# all_fruits.append("pear")
--- question_model.py
+++ question_model.py
@@ -5,6 +5,14 @@ class Question:
 
 
 
+
+#Method syntax example
+    # def follow(self, user):
+    #     user.followers += 1
+    #     self.following += 1
+
+
+
 question_1 = Question("is 2+3=5?", "True")
 
 print(question_1.text)
@@ -12,3 +20,5 @@ print(question_1.text)
 answer = input("Is the answer True or False?")
 if answer == question_1.answer:
     print("You got it correct")
+
+    
