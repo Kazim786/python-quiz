c92b4dbf3e9a0751433bad3eeb19815f102a0c73
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-06 22:21:22 -0600

    did my own stuff

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,18 @@
+from main import question_bank
+
 class QuizBrain:
     def __init__ (self, question_list):
         self.question_number = 0
         self.question_list = question_list
+
+    def next_question(self, text, answer, question_bank):
+        self.question_number += 1
+        for question in question_bank:
+            print(f"Q.{self.question_number}: {question_bank[question].text}")
+            answer = input("(True/False)?")
+            if answer != question_bank[question].answer:
+                print("Incorrect answer")
+
+# a_question = Question(question["text"], question["answer"])
+
+
