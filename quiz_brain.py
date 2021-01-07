ad71f0e42c0b5b441bace2da16592f1cdf3a48c9
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-06 23:37:45 -0600

    end of quiz message

--- quiz_brain.py
+++ quiz_brain.py
@@ -28,7 +28,8 @@ class QuizBrain:
             print("Answer is right")
             self.score +=  1
         print(f"Your score so far is {self.score}/{self.question_number}")
-
+        if self.question_number == len(self.question_list):
+            print(f"You have finished the quiz, your final score is {self.score}")
         
 
 
